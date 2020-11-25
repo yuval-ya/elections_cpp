af933817f986cd97b1aa868fe650d8adbe60f85b
yuval <yakovskind@gmail.com>
2020-11-25 12:31:41 +0200

    Person

--- elections_cpp/Person.cpp
+++ elections_cpp/Person.cpp
@@ -1,1 +1,6 @@
 #include "Person.h"
+
+Person::Person(const char* name, int idnum, int year, int district_num) : _name(name),
+{
+    
+}
--- elections_cpp/Person.h
+++ elections_cpp/Person.h
@@ -1,7 +1,18 @@
 #pragma once
+#include "String.h"
+
 class Person
 {
-
+private:
+    const String _name;
+    const int _id;
+    const int _birth_year;
+    const int _district_num;
+    bool _voted;
+public:
+    Person(const String&, int idnum, int year, int district_num);
+    
+    
 
 };
 
