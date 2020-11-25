55bb1074ff54904736d4910106defb88bfb0e1dd
yuval <yakovskind@gmail.com>
2020-11-25 12:59:15 +0200

    Person class

--- elections_cpp/Person.cpp
+++ elections_cpp/Person.cpp
@@ -1,6 +1,6 @@
 #include "Person.h"
 
-Person::Person(const char* name, int idnum, int year, int district_num) : _name(name),
+Person::Person(const String& name, int idnum, int year, int district_num) :
+_name(name), _id(idnum), _birth_year(year), _district_num(district_num), _voted(false)
 {
-    
 }
--- elections_cpp/Person.h
+++ elections_cpp/Person.h
@@ -11,8 +11,13 @@ private:
     bool _voted;
 public:
     Person(const String&, int idnum, int year, int district_num);
+    ~Person();
     
-    
+    const String& get_name() const { return _name; }
+    int get_id() const { return _id; };
+    int get_year() const { return _birth_year; };
+    int get_district() const { return _district_num; }
+    bool voted() const { return _voted; }
 
 };
 
