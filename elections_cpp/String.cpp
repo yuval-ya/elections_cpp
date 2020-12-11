547c4575b698632a1f6c6861e33f1a894237523a
yuval <yakovskind@gmail.com>
2020-12-11 11:17:27 +0200

    aaa

--- elections_cpp/String.cpp
+++ elections_cpp/String.cpp
@@ -8,33 +8,46 @@ String::String(const char* str) : _str(nullptr)
 
 String::String(const String& str) {
 	const char* tmp = str.get();
-	int size = strlen(tmp);
-	_str = new char[size + 1];
+	_len = strlen(tmp);
+	_str = new char[_len + 1];
     
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < _len; i++) {
 		_str[i] = tmp[i];
 	}
-	_str[size] = '\0';
+	_str[_len] = '\0';
 }
 
 String::~String()
 {
-	delete _str;
+	delete[] _str;
 }
 
 void String::set(const char *str) {
 	if (_str)
 		delete[] _str;
 
-	int size = strlen(str);
-	_str = new char[size + 1];
+	_len = strlen(str);
+	_str = new char[_len + 1];
 
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < _len; i++) {
 		_str[i] = str[i];
 	}
-	_str[size] = '\0';
+	_str[_len] = '\0';
 }
 
+String& String::operator=(String& other){
+    if (this != &other) {
+        if (_str)
+            delete[] _str;
+        
+        _len = other._len;
+        _str = new char[_len + 1];
+        
+        memcpy(_str, other._str, _len);
+        _str[_len] = '\0';
+    }
+    return *this;
+}
 
 ostream& operator<<(ostream& os, const String& str) {
 	os << str._str;
--- elections_cpp/String.h
+++ elections_cpp/String.h
@@ -9,13 +9,16 @@ class String
     
 private:
 	char * _str;
+    long int _len;
 public:
 	String(const char *str);
 	String(const String&);
 	~String();
 
 	const char* get() const { return _str; }
+    long int get_len() const { return _len; }
 	void set(const char *str);
+    String& operator=(String&);
 	friend std::ostream& operator<<(std::ostream& os, const String& str);
 };
 
